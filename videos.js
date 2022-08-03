5fe4ebba5e8488ffe1af7cb574dc8d1624bcb797
lttlq <110307319+lttlq@users.noreply.github.com>
2022-08-03 19:08:54 +0300

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,10 +1,73 @@
 
-const video=document.getElementById('video');
+const video = document.getElementById('video');
+var myContent =[
+   {
+       "software": "Designer V5",
+       "header": [
+           {
+               "name": "Login",
+               "video": [
+                   {
+                       "name": "Login Page",
+                       "url": "#"
+                   },
+                   {
+                       "name": "Register Page",
+                       "url": "#"
+                   }
+               ]
+           },
+           {
+               "name": "Home",
+               "video": [
+                   {
+                       "name": "Home Page",
+                       "url": "#"
+                   },
+                   {
+                       "name": "System Start Page",
+                       "url": "#"
+                   }
+               ]
+           }
+       ]
+   },
+   {
+       "software": "Designer S",
+       "header": [
+           {
+               "name": "Login",
+               "video": [
+                   {
+                       "name": "Login Page",
+                       "url": "#"
+                   },
+                   {
+                       "name": "Register Page",
+                       "url": "#"
+                   }
+               ]
+           },
+           {
+               "name": "Home",
+               "video": [
+                   {
+                       "name": "Home Page",
+                       "url": "#"
+                   },
+                   {
+                       "name": "System Start Page",
+                       "url": "#"
+                   }
+               ]
+           }
+       ]
+   }
+];
 
+function changeVideo(videoName) {
+   video.src = './videos/' + videoName + '.mp4';
+   console.log("video change")
+}
 
-   function changeVideo(videoName) { 
-    video.src = './videos/' + videoName + '.mp4';
-    console.log("video change")
- }
- 
 
