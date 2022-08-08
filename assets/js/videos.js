5be503a977308dc01d66e4db81f52ebb901f85ba
lttlq <110307319+lttlq@users.noreply.github.com>
2022-08-08 12:15:02 +0300

    Update videos.js

--- assets/js/videos.js
+++ assets/js/videos.js
@@ -1,26 +1,39 @@
 var tutorials = new Vue({
     el: '#tutorial',
-    data: { data: CONTENT },
+    data: { data: CONTENT ,lang:"en"},
     methods: {
         changeVideo: function (videoName) {
             var video = document.getElementById("video");
             video.src = './assets/videos/' + videoName + '.mp4';
+        },
+        setLanguage: function (lang){
+            addUrlParameter("lang",lang);
         }
     }
-
 });
-var tutorialsTr = new Vue({
-    el: '#tutorial',
-    data: { data: CONTENTTR },
-    methods: {
-        changeVideo: function (videoName) {
-            var video = document.getElementById("video");
-            video.src = './assets/videos/' + videoName + '.mp4';
+$(document).ready(function() {
+    console.log( "ready!" );
+    tutorials.lang=getQueryVariable("lang");
+    console.log(getQueryVariable("lang"));
+});
+function addUrlParameter(name, value) {
+    var searchParams = new URLSearchParams(window.location.search)
+    searchParams.set(name, value)
+    window.location.search = searchParams.toString();
+  }
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
         }
     }
+    console.log('Query variable %s not found', variable);
+}
 
-});
-
+/*
 var language={
     eng:{tutorials},
     tr:{tutorialsTr}
@@ -35,6 +48,6 @@ if(window.location.hash){
     }
 }
 
-
+*/
 
    
